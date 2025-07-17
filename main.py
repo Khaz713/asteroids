cb167c3193f0484b2c933d89da9b266671f2564f
Khaz713 <konrad713@gmail.com>
2025-07-17 09:13:12 +0200

    added circleshape.py and player.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
-
 import constants
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
