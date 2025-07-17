559a9461c9369fdcf4be34f3ad6814c13c8cc7bc
Khaz713 <konrad713@gmail.com>
2025-07-17 09:35:04 +0200

    updated main.py and player.py, player can now move

--- main.py
+++ main.py
@@ -3,20 +3,34 @@ import constants
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
-    dt = 0 #delta time
+    dt = 0  # delta time
+
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -2,7 +2,9 @@ import constants
 import circleshape
 import pygame
 
+
 class Player(circleshape.CircleShape):
+
     def __init__(self, x, y):
         circleshape.CircleShape.__init__(self, x, y, constants.PLAYER_RADIUS)
         self.rotation = 0
@@ -17,3 +19,21 @@ class Player(circleshape.CircleShape):
 
     def draw(self, screen):
         return pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
+    def rotate(self, dt):
+        self.rotation += dt * constants.PLAYER_TURN_SPEED
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:  # forward
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:  # back
+            self.move(-dt)
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:  # left
+            self.rotate(-dt)
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:  # right
+            self.rotate(dt)
